6c9d93c145faad7ec80b0ea3ac47546650900268
David Aquilue-Llorens <daquilue99@gmail.com>
2025-06-26 17:03:45 +0200

    Solved bug with pip installation in editable mode

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_toolbox",
-    use_scm_version={"write_to": "your_toolbox/_version.py"},
+    name="NeuroBlocks",
+    use_scm_version={"write_to": "neuroblocks/_version.py"},
     setup_requires=["setuptools_scm"],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="David Aquilué-Llorens",
+    author_email="david.aquilue@upf.edu",
     description="Utility functions for neuroimaging and Alzheimer's research",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/your_toolbox",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "matplotlib",
-        "nibabel",
-        "pandas",
-        "scipy",
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

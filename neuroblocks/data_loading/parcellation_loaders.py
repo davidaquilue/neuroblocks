5d9ccf4145408054b040b712e34619060a1d4a5a
David Aquilue-Llorens <daquilue99@gmail.com>
2025-11-19 17:43:53 +0100

    Added BOLD dataloader

--- neuroblocks/data_loading/parcellation_loaders.py
+++ neuroblocks/data_loading/parcellation_loaders.py
@@ -89,3 +89,65 @@ class PETParcellatedLoader(FlexibleBIDSLoader):
                 del simple_dict[subject]
 
         return simple_dict
+
+
+class BOLDParcellatedLoader(FlexibleBIDSLoader):
+    """
+    fMRIParcellatedLoader to load parcellated resting state fMRI BOLD signals.
+
+    :param dir_to_load (str): Path to the directory containing BIDS-structured
+    preprocessed and denoised BOLD data.
+    :param parcellation_str (str): A string identifier for the parcellation scheme
+        used in preprocessing (e.g., 'AAL', 'Schaefer').
+    :param subset_participants (list or None): Optional list of participant IDs to limit
+        the data loading to a subset.
+    :param prepro_prefix (str): string containing the prefix of the preprocessing
+        steps undertaken by conn, defaults to "dswar" (coregistered, aligned,
+        warped, smoothed, denoised).
+    """
+
+    def __init__(
+        self,
+        dir_to_load,
+        parcellation_str,
+        subset_participants=None,
+    ):
+        super().__init__(dir_to_load, subset_participants=subset_participants)
+        self.parcellation_str = parcellation_str
+        bold_re = r"sub-(?P<sub>[^_]+)_ses-(?P<ses>[^_]+)_task-rest_(?P<modality>bold)_"
+        self.pattern = re.compile(bold_re + self.parcellation_str + r".npy")
+
+
+    def get_parcellated_data(self):
+        # Performs a scan of the data, and then goes over all the obtained paths and
+        # loads the parcellated data in numpy format.
+        self._scan()
+        load_numpy_data_to_leaves(self.data)
+        return self.data
+
+    def get_parcellated_data_in_simple_dict(self):
+        """
+        Gets data and returns it in a dictionary of the type:
+        {"sub_id": {"session": np.array(parcellated_data)}, ... }
+
+        :return dict: Nested dictionary {subject_id: {tracer: np.ndarray, ...}, ...}
+        """
+        # Ensure data is scanned and loaded
+        self.get_parcellated_data()
+
+        simple_dict = {}
+        for subject, sessions in self.data.items():  # Iterate over sessions and subs
+            # Initialize per-subject dict
+            simple_dict[subject] = {}
+
+            # Directly load the available sessions
+            for session in sessions:
+                bold_data = sessions.get(session, {}).get("bold", {})
+                if not isinstance(bold_data.get("data"), type(None)):
+                    simple_dict[subject][session] = bold_data.get("data")
+                else:  # Do not add the session to the subject
+                    pass
+            if len(simple_dict[subject]) == 0:  # Remove subjects without data
+                del simple_dict[subject]
+
+        return simple_dict
